d1954cef1b14bdd984f5edf4520f78636a5b67f6
Stephen Thompson <s31@umbc.edu>
2021-11-01 21:24:01 -0500

    create main menu and display employees

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO workers (first_name, last_name, title, department, salary, manager)
+VALUES ("John", "Doe", "Sales Lead", "Sales", 100000, null),
+        ("Mike", "Chan", "Salesperson", "Sales", 80000, "John Doe");
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 require('dotenv').config();
 
 const app = express();
@@ -18,6 +20,43 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_tracker_db database.')
 )
 
-db.query('SELECT * FROM workers', function (err, results) {
-    console.log(results);
-  });
+function showEmployees() {
+    db.query('SELECT * FROM workers', function (err, res) {
+        console.log('\n')
+        console.table(res);
+    });
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'what',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+        },
+    ])
+    .then((data) => {
+        return data.what
+    });
+    
+}
+
+function menu() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'what',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+        },
+    ])
+    .then((data) => {
+        if (data.what === 'View All Employees'){
+            var next = showEmployees();
+        }
+    });
+}
+
+
+menu();
+
